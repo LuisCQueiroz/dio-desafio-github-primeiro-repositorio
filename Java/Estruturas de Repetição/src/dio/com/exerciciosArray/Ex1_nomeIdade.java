3aa038f290975dfd69a6f12f239c47aa21f0f2fd
LuisCQueiroz <djlc2@yahoo.com.br>
2022-05-14 20:18:01 -0300

    Inclusão das aulas Estruturas de Repetição - While

---
+++ Java/Estruturas de Repetição/src/dio/com/exerciciosArray/Ex1_nomeIdade.java
@@ -0,0 +1,39 @@
+package dio.com.exerciciosArray;
+
+/*
+    Exercício 1 - Nome e idade:
+    Faça um programa que leia dois valores:
+    O primeiro representando nome do aluno
+    O segundo representando a idade do aluno
+    Pare se inserir o valor 0 no nome
+
+ */
+
+import java.util.Scanner;
+
+public class Ex1_nomeIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while(true) {
+            System.out.println("Nome: ");
+            nome = scan.next();
+                if (nome.equals("0")) break;
+            System.out.println("idade: ");
+            idade = scan.nextInt();
+        }
+        System.out.println("Continua aqui...");
+
+
+
+
+
+
+
+        }
+    }
+
+

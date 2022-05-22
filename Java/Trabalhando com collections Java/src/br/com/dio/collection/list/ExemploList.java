65b5b29c2bc36a11c8a7be25b425004bd7e0a475
LuisCQueiroz <djlc2@yahoo.com.br>
2022-05-21 23:48:24 -0300

    Adicionando exercício  "Trabalhando com listas" - Java

---
+++ Java/Trabalhando com collections Java/src/br/com/dio/collection/list/ExemploList.java
@@ -0,0 +1,61 @@
+package br.com.dio.collection.list;
+
+// Exemplos de criação de lista
+//        List notas = new ArrayList(); //antes Java 5
+//        List <Double> notas = new ArrayList <Double>(); //Generics jdk5 - Diamond operator jdk 7
+//        ArrayList<Double> notas = new ArrayLista<>();
+//        List <Double> notas = new ArrayList<>(Arrays.asList(7d,8.5,9.3,5d,7d,0d,3.6));
+
+//        List<Double> notas = Arrays.asList(7d,8.5,9.3,5d,7d,0d,3.6);
+//        notas.add(10d);
+//        System.out.println(notas);
+
+//        List<Double> notas = List.of(7d,8.5,9.3,5d,7d,0d,3.6);
+//        notas.add(1d);
+//        notas.remove(o:5d);
+//        System.out.println(notas);
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ExemploList {
+    public static void main(String[] args) {
+        //Dada uma lista com sete notas de um aluno [7,8.5,9.3,5,7,0,3.6]
+        System.out.println("Crie uma lista e adicione as 7 notas");
+
+        List <Double> notas = new ArrayList <Double>();
+        notas.add(7d);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5d);
+        notas.add(7d);
+        notas.add(0d);
+        notas.add(3.6);
+        System.out.println(notas);
+//        System.out.println(notas.toString());
+        System.out.println();
+        System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5d));
+        System.out.println("");
+        System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+        notas.add(4,8.0);
+        System.out.println(notas);
+        System.out.println();
+        System.out.println("Substitua a nota 5.0 pela nota 6.0:");
+        notas.set(notas.indexOf(5d),6.0);
+        System.out.println(notas);
+        System.out.println();
+        System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d));
+        System.out.println();
+        System.out.println("Exiba as notas na ordem que foram informadas: ");
+        for (Double nota:notas) System.out.println(nota);
+
+
+
+
+
+
+
+    }
+
+}

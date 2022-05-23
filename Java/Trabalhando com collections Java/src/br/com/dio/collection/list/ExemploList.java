95e9441247b0c5686808b3b298bef1563da87a62
LuisCQueiroz <djlc2@yahoo.com.br>
2022-05-22 23:58:16 -0300

    Aula 2 de Collections com exercício resolvido

--- Java/Trabalhando com collections Java/src/br/com/dio/collection/list/ExemploList.java
+++ Java/Trabalhando com collections Java/src/br/com/dio/collection/list/ExemploList.java
@@ -15,16 +15,14 @@ package br.com.dio.collection.list;
 //        notas.remove(o:5d);
 //        System.out.println(notas);
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ExemploList {
     public static void main(String[] args) {
         //Dada uma lista com sete notas de um aluno [7,8.5,9.3,5,7,0,3.6]
         System.out.println("Crie uma lista e adicione as 7 notas");
 
-        List <Double> notas = new ArrayList <Double>();
+        List<Double> notas = new ArrayList<Double>();
         notas.add(7d);
         notas.add(8.5);
         notas.add(9.3);
@@ -38,24 +36,65 @@ public class ExemploList {
         System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5d));
         System.out.println("");
         System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
-        notas.add(4,8.0);
+        notas.add(4, 8.0);
         System.out.println(notas);
         System.out.println();
         System.out.println("Substitua a nota 5.0 pela nota 6.0:");
-        notas.set(notas.indexOf(5d),6.0);
+        notas.set(notas.indexOf(5d), 6.0);
         System.out.println(notas);
         System.out.println();
         System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d));
         System.out.println();
         System.out.println("Exiba as notas na ordem que foram informadas: ");
-        for (Double nota:notas) System.out.println(nota);
+        for (Double nota : notas) System.out.println(nota);
+        System.out.println("\nExiba a terceira nota adicionada: " + notas.get(2));
+        System.out.println("\nExiba a menor nota: " + Collections.min(notas));
+        System.out.println("\nExiba a maior nota: " + Collections.max(notas));
+        //Exibir a soma dos elementos;
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()) {
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("\nExiba a soma dos valores: " + soma);
 
+        System.out.println("Exiba a média das notas: " + (soma / notas.size()));
 
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0d);
+        System.out.println(notas);
 
+        System.out.println("Remova a nota da posição 0");
+        notas.remove(0);
+        System.out.println(notas);
 
+        System.out.println("Remova as notas menores que 0 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while(iterator1.hasNext()){
+            Double next = iterator1.next();
+            if(next <7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Apague toda a lista: ");
+        notas.clear();
+        System.out.println(notas);
+
+        System.out.println("Confira se a lista está vazia: " + notas.isEmpty());
+/* Para você resolva os exercícios abaixo usando os métodos da implementação
+        LinkedList */
+        System.out.println("Exercícios");
+        System.out.println("Crie uma lista chamada notas2");
+        // coloque os elementos do ArrayList
+        List<Double> notas2 = new ArrayList<Double>(Arrays.asList(7d,8.5,9.3,5d,7d,0d,3.6));
+        System.out.println(notas2);
+        System.out.println("Mostre a primeira nota da lista sem removê-la: " + notas2.get(0));
+        System.out.println("Mostre a primeira nota da lista removendo-a: " + notas2.get(0) +
+                                                " Removido: " + notas2.remove(notas2.get(0)));
+        System.out.println("Nova lista sem o primeiro: " + notas2);
 
 
 
     }
-
 }
